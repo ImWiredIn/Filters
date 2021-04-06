9bdff586c74141cce5c7f6b0844f6f5ba33c3bc2
ImWiredIn <81716056+ImWiredIn@users.noreply.github.com>
2021-04-05 23:57:22 -0400

    Update and rename OldRedditRedirect to OldRedditRedirect.js

--- OldRedditRedirect.js
+++ OldRedditRedirect.js
@@ -1,6 +1,6 @@
 oldredditredirect.js application/javascript
 (function() 
 {
-    if( /(^|\.)old.reddit\.com$/.test(document.location.hostname))
+    if(/(^|\.)old.reddit\.com$/.test(document.location.hostname)==FALSE)
         document.location = document.URL.replace('www.reddit.com','old.reddit.com');
 })();
