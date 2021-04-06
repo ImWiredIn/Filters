bf03668f7e3e025c8521fa47ae20c98e916ef518
ImWiredIn <81716056+ImWiredIn@users.noreply.github.com>
2021-04-06 00:05:11 -0400

    Update OldRedditRedirect.js

--- OldRedditRedirect.js
+++ OldRedditRedirect.js
@@ -1,6 +1,6 @@
 oldredditredirect.js application/javascript
 (function() 
 {
-    if(/(^|\.)old.reddit\.com$/.test(document.location.hostname)==FALSE)
+    if( /(^|\.)old.reddit\.com$/.test(document.location.hostname) === true ) { return; }
         document.location = document.URL.replace('www.reddit.com','old.reddit.com');
 })();
