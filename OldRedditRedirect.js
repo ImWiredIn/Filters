badb60b44a285ed8f599d57e5481a701e066a27f
ImWiredIn <81716056+ImWiredIn@users.noreply.github.com>
2021-08-12 13:52:30 -0400

    Separate checks for Old, www, and amp links

--- OldRedditRedirect.js
+++ OldRedditRedirect.js
@@ -2,6 +2,14 @@ oldredditredirect.js application/javascript
 (function() 
 {
     if( /(^|\.)old.reddit\.com$/.test(document.location.hostname) === true ) { return; }
+    if( /(^|\.)www.reddit\.com$/.test(document.location.hostname) === true ) 
+    { 
         document.location = document.URL.replace('www.reddit.com','old.reddit.com');
+        return; 
+    }
+    if( /(^|\.)amp.reddit\.com$/.test(document.location.hostname) === true ) 
+    { 
         document.location = document.URL.replace('amp.reddit.com','old.reddit.com');
+        return; 
+    }
 })();
